5e281e9e9010da5bfc47e4a50e1008881dc8643e
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2022-11-20 02:06:16 +0700

    28-fix-bug-preparing-deployment

--- server/server.js
+++ server/server.js
@@ -65,7 +65,14 @@ app.use((err,req,res,next)=>{
     handleError(err,res)
 })
 
+app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
 
+    app.get('/*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 3341;
 app.listen(port, () => {
